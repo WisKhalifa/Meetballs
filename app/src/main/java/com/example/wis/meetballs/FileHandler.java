b15216ea8a6972ec4d26c257a2a6974b1deff4b3
w_halawi@hotmail.co.uk <wisam.h20>
2018-12-01 15:23:05 +0000

    added , between meeting attributes in file added new line after eacg meeting in file added file reader method in filehandler (untested) added recyclerview in homefragment (untested) about to implement recyclerview adapters

--- app/src/main/java/com/example/wis/meetballs/FileHandler.java
+++ app/src/main/java/com/example/wis/meetballs/FileHandler.java
@@ -3,13 +3,19 @@ package com.example.wis.meetballs;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileHandler {
 
 
     private Context context;
+
 
     public FileHandler(Context context) {
         this.context = context;
@@ -18,10 +24,21 @@ public class FileHandler {
 
     public void createMeeting(String title, String notes, String date, String time) {
 
+
         try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("meetings.txt", Context.MODE_PRIVATE));
+
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput
+                    ("meetings.txt", Context.MODE_APPEND));
             outputStreamWriter.write(title);
+            outputStreamWriter.write(",");
+            outputStreamWriter.write(notes);
+            outputStreamWriter.write(",");
+            outputStreamWriter.write(date);
+            outputStreamWriter.write(",");
+            outputStreamWriter.write(time);
+            outputStreamWriter.write("\n");
             outputStreamWriter.close();
+
 
         } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
@@ -30,5 +47,26 @@ public class FileHandler {
 
 
     }
+
+    public List<String> displayMeeting() {
+        List<String> meeting = new ArrayList<>();
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(context.openFileInput("meetings.txt"));
+            BufferedReader r = new BufferedReader(inputStreamReader);
+            StringBuilder total = new StringBuilder();
+            for (String line; (line = r.readLine()) != null; ) {
+                total.append(line).append('\n');
+                meeting.add(line);
+            }
+            System.out.println(Arrays.toString(meeting.toArray()));
+            r.close();
+
+        } catch (IOException e) {
+
+        }
+
+        return meeting;
+    }
+
 
 }
